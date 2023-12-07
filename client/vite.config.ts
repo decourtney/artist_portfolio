7d510d29209fa14c8309210aaa936ed8b4499aef
decourtney <donovan.courtney@gmail.com>
2023-12-07 15:43:04 +0000

    testing relit commit/push

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   base: "/",
   plugins: [react(), viteTsconfigPaths()],
   server: {
+    host: "0.0.0.0",
     proxy: {
       "/graphql": {
         target: "http://localhost:3000",
